c241242eac174fc5d100081d78cd5e5f9e24c84c
Saehanseul <saehanseul@gmail.com>
2025-06-10 13:34:01 +0900

    temp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+"use client";
+
 import ModelMatchingLatestChatList from "@/components/model-matching-latest-chat-channels";
 
 export default function Home() {
   return (
     <div>
+      aaa
       <ModelMatchingLatestChatList />
     </div>
   );
