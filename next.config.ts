d7734b3178e7015cb09d9f606d2da08344916ac8
Saehanseul <saehanseul@gmail.com>
2025-06-10 13:44:56 +0900

    feat: 리디렉션 수정

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactStrictMode: true
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/latest-chat-list",
+        permanent: false
+      }
+    ];
+  }
 };
 
 export default nextConfig;
